251192e03e44cd6318eb711f3999a96643ca5a98
leonardo mazzoleni <leonardo.mazzoleni@tecnasoft.it>
2024-11-05 15:23:51 +0100

    test 6

--- src/Queries/Admin/Catalog/Products/ProductContent.php
+++ src/Queries/Admin/Catalog/Products/ProductContent.php
@@ -19,7 +19,7 @@ use Webkul\BookingProduct\Helpers\TableSlot as TableSlotHelper;
 class ProductContent extends BaseFilter
 {
     /**
-     * Create a new controller instance2.
+     * Create a new controller instance3.
      *
      * @return void
      */
